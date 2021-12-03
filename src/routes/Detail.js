85080738c033be3c6d6d82194d1b743e796019c2
MinhaLee94 <shjune1994@gmail.com>
2021-12-02 23:57:56 -0500

    Build detail page

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,20 @@
-export default () => "DETAIL";
+import React from "react";
+import { connect } from "react-redux";
+
+const Detail = ({toDo}) => {
+	return (
+		<h1>{toDo.text}</h1>
+	);
+}
+
+function mapStateToProps(state, ownProps) {
+	const { 
+		match: { 
+			params: { id }
+		}
+	} = ownProps;
+	console.log(ownProps);
+	return { toDo: state.find(toDo => toDo.id === parseInt(id)) };
+}
+
+export default connect(mapStateToProps)(Detail);
